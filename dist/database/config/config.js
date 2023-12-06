a244ec0037b00164d0c14a603c1e0337f7b0c9d8
Kévin Hersin <khersin@auto-ici.fr>
2023-12-06 16:12:38 +0100

    test

---
+++ dist/database/config/config.js
@@ -0,0 +1,11 @@
+"use strict";
+require('dotenv').config();
+module.exports = {
+    dialect: "mysql",
+    host: "localhost",
+    username: "root",
+    password: "",
+    database: "testTs",
+    logging: false, // Disables logging of SQL queries
+    models: []
+};
